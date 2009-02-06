029f6ad83c8b1b2445876a0aadcab6d0ddfa9677
Erik Hollembeak <erik.hollembeak@gmail.com>
2009-02-06 11:15:22 +0800

    Added exception to handle no missile launcher present.
    Signed-off-by: Jon Olson <jon@damogran.com>

--- calibrate.py
+++ calibrate.py
@@ -9,10 +9,11 @@ points = []
 manager = RocketManager()
 manager.acquire_devices()
 
-rocket = manager.launchers[0]
-
-if not rocket:
-	raise "No rocket launchers"
+try:
+   rocket = manager.launchers[0]
+except: IndexError:
+   print "No rocket launchers"
+   exit(1)
 
 DOWN = 0
 UP = 1
@@ -20,34 +21,34 @@ LEFT = 2
 RIGHT = 3
 
 def wait_for(direction):
-	"""docstring for wait_for"""
-	limits = (False,)*4
-	while not limits[direction]:
-		limits = rocket.check_limits()
+   """docstring for wait_for"""
+   limits = (False,)*4
+   while not limits[direction]:
+      limits = rocket.check_limits()
 
 def mean(collection):
-	"""docstring for mean"""
-	return float(sum(collection)) / len(collection)
+   """docstring for mean"""
+   return float(sum(collection)) / len(collection)
 
 
 for x in range(1):
-	rocket.start_movement(DOWN)
-	wait_for(DOWN)
+   rocket.start_movement(DOWN)
+   wait_for(DOWN)
 
-	start_vert = time()
-	rocket.start_movement(UP)
-	wait_for(UP)
-	stop_vert = time()
+   start_vert = time()
+   rocket.start_movement(UP)
+   wait_for(UP)
+   stop_vert = time()
 
-	rocket.start_movement(LEFT)
-	wait_for(LEFT)
+   rocket.start_movement(LEFT)
+   wait_for(LEFT)
 
-	start_horiz = time()
-	rocket.start_movement(RIGHT)
-	wait_for(RIGHT)
-	stop_horiz = time()
+   start_horiz = time()
+   rocket.start_movement(RIGHT)
+   wait_for(RIGHT)
+   stop_horiz = time()
 
-	points.append((stop_vert - start_vert, stop_horiz - start_horiz))
+   points.append((stop_vert - start_vert, stop_horiz - start_horiz))
 
 mean_vert = mean([vert for vert, horiz in points])
 mean_horiz = mean([horiz for vert, horiz in points])
@@ -67,4 +68,4 @@ sleep(mean_horiz / 2.0)
 rocket.stop_movement()
 
 for p in points:
-	print p
+   print p
